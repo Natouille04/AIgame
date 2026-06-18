625bbab17026207d356287bda24e0b52a8d7b1be
Natouille04 <nathan.gouveia2008@gmail.com>
2026-06-18 23:33:35 +0200

    Created home page and stylised it

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-// Expose des APIs sécurisées au renderer via window.electron
-contextBridge.exposeInMainWorld('electron', {
-  ping: () => ipcRenderer.invoke('example:ping'),
-  // Ajouter ici les fonctions IPC supplémentaires
-})
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">AIgame</h1>
-        <p className="text-gray-400">App prête pour le code !</p>
+    <div className="w-full h-screen flex flex-col items-center gap-y-15 justify-center background-pattern bg-gray-900 text-white">
+      <div className="w-70 flex flex-col select-none">
+        <p className="text-5xl font-bold CascadiaCode self-start">Promptly</p>
+        <p className="text-5xl font-bold CascadiaCode self-end">Executed</p>
       </div>
+
+      <div className="flex flex-col gap-4">
+        <button className="w-50 h-10 transition cursor-pointer CourierPrime font-bold text-white tracking-widest py-1 px-4 rounded-xl bg-linear-to-b from-[#1a1433] to-[#470bb8] border-2 border-violet-400">CREATE ROOM</button>
+        <button className="w-50 h-10 transition cursor-pointer CourierPrime font-bold text-white tracking-widest py-1 px-4 rounded-xl bg-linear-to-b from-[#1a1433] to-[#470bb8] border-2 border-violet-400">JOIN ROOM</button>
+      </div>
+
+      <p className="absolute bottom-5 left-1/2 -translate-x-1/2 text-sm tracking-wide opacity-80 font-light">
+        Copyright Nouille and Mania © {new Date().getFullYear()}
+      </p>
     </div>
   )
 }
